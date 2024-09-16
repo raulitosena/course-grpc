58577ca4e61edd6d88361b471d70f386cf4d3765
Raulito Sena <raulitosena@yahoo.com.br>
2024-09-16 01:38:37 +0000

    08-sum-callback-custom - SumReactor implementation.

---
+++ 08-sum-callback-custom/src/client.cpp
@@ -0,0 +1,84 @@
+#include <grpcpp/grpcpp.h>
+#include <proto/sum.pb.h>
+#include <proto/sum.grpc.pb.h>
+#include <iostream>
+#include <condition_variable>
+#include <mutex>
+
+
+class SumClient {
+public:
+	explicit SumClient(std::shared_ptr<grpc::Channel> channel) : stub(sum::SumService::NewStub(channel)) 
+	{		
+	}
+
+	int ComputeSum(int op1, int op2) 
+	{
+		grpc::ClientContext context;
+		sum::SumOperand request;
+		sum::SumResult response;
+		request.set_op1(op1);
+		request.set_op2(op2);
+		this->stub->async()->ComputeSum(&context, &request, &response, std::bind(&SumClient::ComputeSumDone, this, std::placeholders::_1));
+
+		std::unique_lock<std::mutex> lock(this->mtx);
+		while (!done) 
+		{
+			this->cv.wait(lock);
+		}
+
+		if (this->status.ok()) 
+		{
+			return response.result();
+		} 
+		else 
+		{
+			throw std::runtime_error(this->status.error_message());
+		}
+	}
+
+	void ComputeSumDone(grpc::Status status)
+	{
+		this->status = std::move(status);
+		std::unique_lock<std::mutex> lock(mtx);
+		this->done = true;
+		this->cv.notify_one();
+	}
+
+private:
+	std::unique_ptr<sum::SumService::Stub> stub;
+	std::mutex mtx;
+	std::condition_variable cv;
+	bool done = false;
+	grpc::Status status;
+};
+
+int main(int argc, char** argv) 
+{
+	std::cout << "..:: 08-sum-callback-custom ::.." << std::endl;
+
+	if (argc != 4)
+	{
+		std::cerr << "Usage: ./client <port> <op1> <op2>" << std::endl;
+		return 1001;
+	}
+
+	try
+	{
+		int port = std::stoi(argv[1]);
+		int op1 = std::stoi(argv[2]);
+		int op2 = std::stoi(argv[3]);
+		std::string host = absl::StrFormat("localhost:%d", port);
+
+		auto channel = grpc::CreateChannel(host, grpc::InsecureChannelCredentials());
+		SumClient client(channel);
+		int result = client.ComputeSum(op1, op2);	
+		std::cout << op1 << " + " << op2 << " = " << result << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	return 0;
+}
---
+++ 08-sum-callback-custom/src/server.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <grpcpp/grpcpp.h>
+#include <proto/sum.pb.h>
+#include <proto/sum.grpc.pb.h>
+
+
+class SumReactor : public grpc::ServerUnaryReactor 
+{
+public:
+	SumReactor(const sum::SumOperand& request, sum::SumResult* response) 
+	{
+		response->set_result(request.op1() + request.op2());
+		this->Finish(grpc::Status::OK);
+	}
+
+private:
+	void OnDone() override 
+	{
+		std::cout << "RPC Completed" << std::endl;
+		delete this;
+	}
+
+	void OnCancel() override 
+	{
+		std::cerr << "RPC Cancelled" << std::endl;
+	}
+};
+
+class SumServiceImpl : public sum::SumService::CallbackService
+{
+public:
+	grpc::ServerUnaryReactor* ComputeSum(grpc::CallbackServerContext* context, const sum::SumOperand* request, sum::SumResult* response) override
+	{
+		return new SumReactor(*request, response);
+	}
+};
+
+int main(int argc, char** argv)
+{
+	std::cout << "..:: 08-sum-callback-custom ::.." << std::endl;
+
+	if (argc != 2)
+	{
+		std::cerr << "Usage: ./server <port>" << std::endl;
+		return 1001;
+	}
+	
+	try
+	{
+		int port = std::stoi(argv[1]);
+		std::string host = absl::StrFormat("localhost:%d", port);
+
+		SumServiceImpl service;
+		grpc::ServerBuilder builder;
+		builder.AddListeningPort(host, grpc::InsecureServerCredentials());
+		builder.RegisterService(&service);
+		std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+		std::cout << "Sum server running on " << host << " ..." << std::endl;
+
+		server->Wait();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	return 0;
+}
